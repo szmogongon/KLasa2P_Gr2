6e10d2379bebec8128464b21521ccc36a9d1036a
Kubolowe <kubolowe.priv@gmail.comgit config --global user.name Kubolowegit config --global user.email kubolowe.priv@gmail.com>
2023-05-15 12:17:47 +0200

    wersja 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KLasa 2P");
+            Console.WriteLine("Grupa 2");
+            Console.ReadLine();
+        }
+    }
+}
